cfd5aae5f339e0f31f5c42740b61980a9f157079
Rafli Ramadhani <rafliramadhani438@gmail.com>
2022-11-05 17:13:52 +0700

    selesai di fitur hero

---
+++ script.js
@@ -0,0 +1,11 @@
+$(window).on('load', function () {
+  $('.hero .text1').css({
+    opacity: '1',
+    transform: 'translateX(0)',
+  });
+
+  $('.hero .img-hero').css({
+    opacity: '1',
+    transform: 'translateX(0)',
+  });
+});
